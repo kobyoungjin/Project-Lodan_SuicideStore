4e3949d3754ce1dc2143e6be4754a2851eacabab
prideofh <68545121+prideofh@users.noreply.github.com>
2021-07-12 11:34:35 +0900

    새로운 업데이트ㅎ
    ㅎㅎ

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Á¹¸³´Ù JEUK
     }
 
     // Update is called once per frame
