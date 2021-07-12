7cf1511c44995e310c908500da609984d2d0a396
Sophriel <32302066+Sophriel@users.noreply.github.com>
2021-07-12 11:17:22 +0900

    first commit
    1차 예시 커밋

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	//  게임을 총괄할 게임매니저 스크립트입니다.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
